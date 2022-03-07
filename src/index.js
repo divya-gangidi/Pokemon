f9be4122b35c4dc6fa1bbb669488da65d6ce87b7
divya-gangidi <divyag.05@gmail.com>
2022-03-07 14:09:40 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 $(document).ready(()=>{
-    console.log("on load")
-    $("#pokemonDiv").hide()
-    $('.carousel-div').hide()
+   
+    hideOnload()
+    //localStorage.clear()
     
 
     $("#searchButton").click(()=>{
@@ -26,29 +26,55 @@ $(document).ready(()=>{
         addFavoritePokemon()
     })
 
-    $( "#searchButton" ).click();
-
+    loadPokemons()
+    //testLoad()
     
   });
 
-  const savedPokemons = []
+  let savedPokemons = []
   let currentData;
+
+  hideOnload =()=>{
+    $("#pokemonDiv").hide()
+    $('.carousel-div').hide()
+    $('.error-div').hide()
+    $('.template-remove').hide()
+  }
+
+  loadPokemons =()=>{
+
+    for(var i=0;i<localStorage.length; i++) {
+      var key = localStorage.key( i );
+      var item = JSON.parse( localStorage.getItem( key ) );
+      console.log(item)
+      currentData = item
+      addClonedDiv()
+    }
+
+  }
+
 
   async function getPokemonDetails(searchValue) {
 
     let response = await fetch('https://pokeapi.co/api/v2/pokemon/'+searchValue);
-    let data = await response.json(); 
-    loadData(data)
+    try{
+      let response = await fetch('https://pokeapi.co/api/v2/pokemon/'+searchValue);
+      let data = await response.json(); 
+      loadData(data)
+    } catch(e){
+      $("#pokemonDiv").hide()
+      $('.error-div').show()
+    }
     
   }
   
 
-  let loadData =(data)=>{
+  loadData =(data)=>{
       $("#imageBack").hide()
       $("#imageFront").show()
       
     console.log(data)
-    console.log(data.sprites.front_default)
+    //console.log(data.sprites.front_default)
     $("#imageFront").attr("src",data.sprites.front_default);
 
     $("#imageBack").attr('src',data.sprites.back_default);
@@ -56,29 +82,71 @@ $(document).ready(()=>{
     $("#order").html(data.order)
     $("#baseExperience").html(data.base_experience)
     $("#pokemonDiv").show()
+    $('.error-div').hide()
 
 
     currentData = data
   }
 
-  let addFavoritePokemon = ()=>{
-    console.log("add to favorite click")
+  addFavoritePokemon = ()=>{
+    //console.log("add to favorite click")
+    
+    addClonedDiv()
     let name = currentData.species.name
-    let spanElement = $("#span-template").clone()
-    spanElement.attr('id','span-'+name)
-    spanElement.find('img').attr('src',currentData.sprites.front_default).attr('id',name)
-    spanElement.find('span').html(name)
-    $('.carousel-div').append(spanElement)
-    $('.carousel-div').show()
-
-    savedPokemons.push({"name":name, "data":currentData})
+    localStorage.setItem(name, JSON.stringify(currentData));
   }
 
 
-  let loadPokemon = (e) =>{
+  addClonedDiv =()=>{
+    let name = currentData.species.name
+    let spanElement = $("#span-template").clone()
+    spanElement.attr('id','span-'+name)
+    spanElement.find('img').attr('src',currentData.sprites.front_default)
+              .attr('id',name)
+              .attr('title',name);
+    spanElement.find('span').attr('class','remove').show();
+    $('.carousel-div').append(spanElement)
+    $('.carousel-div').show()
+  }
+
+
+
+  loadPokemon = (e) =>{
     
     let pokemonName = e.target.id
     console.log(pokemonName)
 
+    let data = localStorage.getItem(pokemonName);
+    loadData(JSON.parse(data))
+  }
 
+  removePokemon =(e)=>{
+    let pokemonName = $(e.target).siblings('img').attr('id')
+    console.log(pokemonName)
+    //console.log(savedPokemons)
+    localStorage.removeItem(pokemonName)
+    // savedPokemons = savedPokemons.filter(pk => pk.name !== pokemonName)
+    //console.log(savedPokemons)
+    $('#span-'+pokemonName).remove()
+    if(currentData.species.name == pokemonName ){
+      $("#pokemonDiv").hide()
+    }
+  }
+
+testLoad = ()=>{
+    
+    $("#search").val('pikachu')
+    $( "#searchButton" ).click();
+    
+    setTimeout(()=>{
+      $("#addFavorite").click();
+      $("#search").val('paras')
+      $( "#searchButton" ).click();
+    }, 1000);
+
+    setTimeout(()=>{
+      $("#addFavorite").click();
+      $("#search").val('ditto')
+      $( "#searchButton" ).click();
+    }, 2000);
   }
